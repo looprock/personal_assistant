72e2a8b2e964e510a7e0124c068045e05d665832
Douglas Land <dland@homestoryrewards.com>
2026-03-16 16:11:16 -0500

    debug output

--- bin/reinit.sh
+++ bin/reinit.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash -x
+launchctl list |grep personal                                                                                                                                          09:51:41
 echo "#### unloading"
 # launchctl unload ~/Library/LaunchAgents/com.personalassistant.ui.plist                                                                                              10s 09:51:08
 launchctl bootout gui/$(id -u)/com.personalassistant.ui
