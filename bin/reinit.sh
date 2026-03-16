63c2d7350d91e1b2704b78d2d45ea90a787bbd0f
Douglas Land <dland@homestoryrewards.com>
2026-03-16 16:07:01 -0500

    update packages and reinit

--- bin/reinit.sh
+++ bin/reinit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "#### unloading"
-launchctl unload ~/Library/LaunchAgents/com.personalassistant.ui.plist                                                                                              10s 09:51:08
+# launchctl unload ~/Library/LaunchAgents/com.personalassistant.ui.plist                                                                                              10s 09:51:08
+launchctl bootout gui/$(id -u)/com.personalassistant.ui
 launchctl list |grep personal                                                                                                                                           09:51:41
 echo "#### loading"
-launchctl load ~/Library/LaunchAgents/com.personalassistant.ui.plist
+# launchctl load ~/Library/LaunchAgents/com.personalassistant.ui.plist
+launchctl bootstrap gui/$(id -u) ~/Library/LaunchAgents/com.personalassistant.ui.plist
 launchctl list |grep personal
